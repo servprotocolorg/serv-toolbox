47e344b199ea7e6aaa77e110d8afb3cab5099edc
patrickmogul <patrick@easynode.pro>
2023-08-24 09:06:32 -0500

    custom serv.sh

--- src/bin/serv.sh
+++ src/bin/serv.sh
@@ -2,19 +2,19 @@
 HOME_DIR=$(echo ~)
 
 # Check if the harmony-toolbox directory exists
-if [ -d "$HOME_DIR/harmony-toolbox" ]; then
+if [ -d "$HOME_DIR/serv-toolbox" ]; then
   # If it exists, go into it and pull updates
-  cd ~/harmony-toolbox
+  cd ~/serv-toolbox
   git pull --quiet > /dev/null 2>&1
 else
   # If it doesn't exist, clone the repository
-  git clone https://github.com/easy-node-pro/harmony-toolbox.git ~/harmony-toolbox > /dev/null 2>&1
+  git clone https://github.com/ServProtocolOrg/serv-toolbox.git ~/serv-toolbox > /dev/null 2>&1
   # Go into the new directory, we already have updates
-  cd ~/harmony-toolbox
+  cd ~/serv-toolbox
 fi
 
 # Install requirements for both
 pip3 install -r requirements.txt --quiet > /dev/null 2>&1
 
 # Start toolbox, with flags if passed
-python3 ~/harmony-toolbox/src/menu.py "$@"
+python3 ~/serv-toolbox/src/app.py "$@"
