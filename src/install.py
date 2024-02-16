957930572e3d1f4a3f3ce7b49e39b70cb6c6c0d7
Patrick Mogul <patrick@easynode.pro>
2024-02-16 17:36:33 -0600

    Test

--- src/install.py
+++ src/install.py
@@ -13,8 +13,24 @@ def install_check() -> None:
     print_stars()
     print("* Checking File Configuration")
     print_stars()
-    if os.path.isdir(config.serv_dir) and os.path.isdir(config.serv_config_dir) and os.path.isfile(config.serv_conf) and os.path.isdir(config.toolbox_location):
+    if os.path.isdir(config.serv_dir):
         print(f"* SERV Node is installed at {config.serv_dir}")
+    else:
+        print(f"* SERV Node is not installed at {config.serv_dir}")
+    if os.path.isdir(config.serv_config_dir):
+        print(f"* SERV Config is installed at {config.serv_config_dir}")
+    else:
+        print(f"* SERV Config is not installed at {config.serv_config_dir}")
+    if os.path.isfile(config.serv_conf):
+        print(f"* SERV Config File is installed at {config.serv_conf}")
+    else:
+        print(f"* SERV Config File is not installed at {config.serv_conf}")
+    if os.path.isdir(config.toolbox_location):
+        print(f"* SERV Toolbox is installed at {config.toolbox_location}")
+    else:
+        print(f"* SERV Toolbox is not installed at {config.toolbox_location}") 
+    if os.path.isdir(config.serv_dir) and os.path.isdir(config.serv_config_dir) and os.path.isfile(config.serv_conf) and os.path.isdir(config.toolbox_location):
+        print(f"* SERV Node is already installed at {config.serv_dir}")
     else:
         print(f"* SERV Node is not installed at {config.serv_dir}")
     print_stars()
